4636e403e34e85a1e8550e112d6ed01667391be6
Vincent Servio <vincentservio@gmail.com>
2020-02-25 21:03:46 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
+require 'pry'
 class BankAccount
+  attr_reader :name
+attr_accessor :BankAccount, :balance, :status
+
+def initialize(name) 
+ # binding.pry
+   @name = name 
+  @bank_account 
+  @balance = 1000
+  @status = "open"
+end 
+
+def deposit(amount) 
+  self.balance += amount
+end
+
+def display_balance 
+  "Your balance is $#{self.balance}."
+end
+
+def valid?
+  balance > 0 &&  status == "open"
+end
+
+def close_account
+  self.status = "closed"
+ # binding.pry
+end
 
 end
