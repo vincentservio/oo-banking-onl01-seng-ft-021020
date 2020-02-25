8c6ec1d17eda0949d37adc041a27bf474e667d72
Vincent Servio <vincentservio@gmail.com>
2020-02-25 21:06:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,7 +25,6 @@ class Transfer
 
     elsif @status == "complete"
       puts "Transaction was already excuted"
-    else
       @sender.deposit( @amount * -1 ) 
       @receiver.deposit( @amount )
       @status = "complete"
