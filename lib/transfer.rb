3dd67225f0bffff1c6bcbaa25a82370a96abdb94
Vincent Servio <vincentservio@gmail.com>
2020-02-25 21:56:06 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,20 +20,20 @@ class Transfer
   def execute_transaction
     #binding.pry
    if valid? && sender.balance > amount && self.status == "pending"
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
      
+     elsif @status == "complete"
+      puts "Transaction was already excuted"
      
-     
-     
+   else
      
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
-    elsif @status == "complete"
-      puts "Transaction was already excuted"
-    else
-      @sender.deposit( @amount * -1 ) 
-      @receiver.deposit( @amount )
-      @status = "complete"
+    
+     
     end
   end
 
