6b6db21b8625d84c1303dfb33acf8eaa2df86e34
Vincent Servio <vincentservio@gmail.com>
2020-02-25 21:22:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
 
   def execute_transaction
     #binding.pry
-    if @sender.balance < @amount || @sender.status == 
+    if @sender.balance < @amount || @sender.status == "closed"
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
